75042cb7b193185043db4b59ce2491a6936fbb61
Gabriel Alves <37886654+GabrielDragone@users.noreply.github.com>
2023-01-09 20:06:46 -0300

    Inicio de desenvolvimento do exemplo e finalização do primeiro topico da aula

---
+++ frontend/quadrofront/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListaNoticiasComponent } from './lista-noticias/lista-noticias.component';
+import { DetalheNoticiaComponent } from './detalhe-noticia/detalhe-noticia.component';
+
+// Tem cada rota da aplicação SPA:
+const routes: Routes = [
+  { path: '', component: ListaNoticiasComponent },
+  { path: 'detalhes', component: DetalheNoticiaComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
