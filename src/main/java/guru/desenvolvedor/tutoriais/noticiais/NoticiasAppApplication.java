42293339250ac8fefec51303224b0a471b7810ad
Gabriel Alves <37886654+GabrielDragone@users.noreply.github.com>
2023-01-09 17:55:35 -0300

    first commit

---
+++ src/main/java/guru/desenvolvedor/tutoriais/noticiais/NoticiasAppApplication.java
@@ -0,0 +1,13 @@
+package guru.desenvolvedor.tutoriais.noticiais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticiasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticiasAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/desenvolvedor/tutoriais/noticiais/NoticiasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.desenvolvedor.tutoriais.noticiais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticiasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
